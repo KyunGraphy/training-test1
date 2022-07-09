f044ecf05e120561aad94a018432c3ef95a19a6c
KyunGraphy <onhoan0@gmail.com>
2022-07-09 18:27:22 +0700

    Kien|Adjust|Fetch API from JSON Server and show data on view

--- client/src/Components/Main/Project.js
+++ client/src/Components/Main/Project.js
@@ -2,17 +2,19 @@ import React from 'react'
 import './Project.css'
 
 function Project({ project }) {
-  return (
-    <div className='project'>
-        <img className='projectImage' src='https://firebasestorage.googleapis.com/v0/b/socialject-alpha-1.appspot.com/o/images%2F%C4%90%E1%BB%93ng%20Kh%C6%A1i.jpg?alt=media&token=42943827-6fb2-4345-9321-d4ba6bc328c3'/>
-        <img className='projectAva' src='https://lh3.googleusercontent.com/a-/AOh14GhoEu9U8ce_FBpNtNTPj5e1G_9VWOryds6WcT_r'/>
-        <div className='projectText'>
-          <div className='projectTitle'>Digital Logic Design Project</div>
-          <div className='projectAuthor'>by Mogzzer</div>
-        </div>
-        <div className='projectDdl'>Deadline: Tomorrow</div>
-    </div>
-  )
+
+    return (
+      <div className='project'>
+          <img className='projectImage' src={project.image}/>
+          <img className='projectAva' src={project.ava}/>
+          <div className='projectText'>
+            <div className='projectTitle'>{project.title}</div>
+            <div className='projectAuthor'>by {project.author}</div>
+          </div>
+          <div className='projectDdl'>Deadline: {project.deadline}</div>
+      </div>
+    )
+
 }
 
 export default Project
--- client/src/Components/Main/ProjectReel.js
+++ client/src/Components/Main/ProjectReel.js
@@ -1,39 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './ProjectReel.css'
 import Project from './Project'
 
 function ProjectReel({ prjAPI }) {
-  var projects
+    const [projects, setProjects] = useState([])
 
-  axios.get(prjAPI)
-    .then(res => {
-      projects = res.data;
-      console.log(projects);
-    })
-    .catch(error => console.log(error));
-  
-  // setTimeout(() =>{
-  //   console.log(projects);
-  // }, 3000)
-  return (
+    useEffect(() => {
+        axios.get(prjAPI)
+          .then(res => {
+            setProjects(res.data);
+            console.log(projects);
+          })
+          .catch(error => console.log(error));
+    }, [])
+
+    return (
     <div className='projectReel'>
-      {/* {
-        projects.map(project => {
-          <List 
+      {projects.map(project => (
+          <Project 
             key={project.id}
             project={project}
           />
-        })
-      } */}
+      ))}
 
+      {/* <Project />
       <Project />
       <Project />
       <Project />
       <Project />
       <Project />
-      <Project />
-      <Project />
+      <Project /> */}
     </div>
   )
 }
