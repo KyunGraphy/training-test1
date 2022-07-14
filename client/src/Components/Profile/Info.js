28aff2db70a5533fab4b18ab242d435034bc1acb
KyunGraphy <onhoan0@gmail.com>
2022-07-15 00:02:47 +0700

    Kien|Feature|Set up profile Page

---
+++ client/src/Components/Profile/Info.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Info() {
+    return (
+        <div className='info'>
+            <div className='generic'></div>
+            <div className='contact'></div>
+        </div>
+    )
+}
+
+export default Info
---
+++ client/src/Components/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Profile.css'
+
+function Profile() {
+    return (
+        <div className='profile'>
+            <div className='profileInfo'>Hi</div>
+            <div className='container'>Hello</div>
+        </div>
+    )
+}
+
+export default Profile
