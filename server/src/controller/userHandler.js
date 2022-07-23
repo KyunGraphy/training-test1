2645eb8e7cc6223bf75f57f11b2cdde1a5cda88e
unknown <phamchaugiatu123@gmail.com>
2022-07-23 21:43:51 +0700

    Anh|Fix|feture login user

--- server/src/controller/userHandler.js
+++ server/src/controller/userHandler.js
@@ -47,9 +47,9 @@ module.exports = {
                 userPassword
             } = req.body
             console.log(req.body);
-            let user = await User.findOne(
-                {userName: userName}
-            ).lean();
+            let user = await User.findOne({
+                userName: userName
+            }).lean();
             if (!user) {
                 return res.status(400).json({
                     message: "User already exits!"
